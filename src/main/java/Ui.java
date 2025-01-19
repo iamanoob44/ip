a4af69fc337a3dc0ff327a5eeb5b2b25e8dbdbb4
iamanoob44 <e1121904@u.nus.edu>
2025-01-19 18:56:16 +0800

    Refine greeting and exit

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -14,19 +14,19 @@ public class Ui {
      * Returns the greeting.
      */
     public void printGreeting() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm " + botName);
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("     ____________________________________________________________");
+        System.out.println("     Hello! I'm " + botName);
+        System.out.println("     What can I do for you?");
+        System.out.println("     ____________________________________________________________");
     }
 
     /*
      * Returns the exit message.
      */
     public void printExit() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("     ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("     ____________________________________________________________");
     }
 
     /**
@@ -35,9 +35,9 @@ public class Ui {
      * @param input input by user to echo.
      */
     public void echo(String input) {
-        System.out.println("____________________________________________________________");
-        System.out.println(input);
-        System.out.println("____________________________________________________________");
+        System.out.println("     ____________________________________________________________");
+        System.out.println("     " + input);
+        System.out.println("     ____________________________________________________________");
     }
-    
+
 }
