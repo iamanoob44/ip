0dd2a3622f94ff7f6fb192078c29a72a2058bc54
iamanoob44 <e1121904@u.nus.edu>
2025-01-19 17:47:45 +0800

    Rename Duke.java to Shagbot.java

--- src/main/java/Shagbot.java
+++ src/main/java/Shagbot.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Shagbot {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,10 +7,9 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
     }
-
-
-    
 }
+
+
 
 
 
