d940bf6dc91d7b7f708ac64610602eab3a195952
iamanoob44 <e1121904@u.nus.edu>
2025-01-19 18:58:20 +0800

    Incorporate echo feature in Shagbot class

--- src/main/java/Shagbot.java
+++ src/main/java/Shagbot.java
@@ -1,7 +1,44 @@
+import java.util.Scanner;
+
 public class Shagbot {
+
+    private final String botName;
+    private final Ui ui;
+
+    /**
+     * Constructor for Shagbot
+     *
+     * @param name the name of the chatbot.
+     */
+    public Shagbot(String name) {
+        botName = name;
+        this.ui = new Ui(name);
+    }
+
+    /**
+     * Starts the chatbot and handles user input.
+     */
+    public void start() {
+        ui.printGreeting();
+        Scanner scanner = new java.util.Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine().trim();
+            if (userInput.equalsIgnoreCase("Bye")) {
+                ui.printExit();
+                break;
+            } else {
+                ui.echo(userInput);
+            }
+        }
+        scanner.close();
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.print(printGreeting());
-        System.out.print(printExit());
+        Shagbot shagbot = new Shagbot("Shagbot");
+        shagbot.start();
     }
 
     /*
