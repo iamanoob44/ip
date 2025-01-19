d8c12ed8fb6a735799bfe1182bdef8aeb839c9dd
iamanoob44 <e1121904@u.nus.edu>
2025-01-19 18:07:46 +0800

    Add greeting and exit

--- src/main/java/Shagbot.java
+++ src/main/java/Shagbot.java
@@ -1,12 +1,23 @@
 public class Shagbot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.print(printGreeting());
+        System.out.print(printExit());
     }
+
+    /*
+     * Returns the greeting.
+     */
+    public static String printGreeting() {
+        return "Hello! I'm Shagbot\n" + "What can I do for you?\n";
+    }
+
+    /*
+     * Returns the exit message.
+     */
+    public static String printExit() {
+        return "Bye. Hope to see you again soon!\n";
+    }
+
 }
 
 
