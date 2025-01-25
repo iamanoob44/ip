a368d96ec16a38674393b1fef17aa31e90aab95c
iamanoob44 <e1121904@u.nus.edu>
2025-01-25 15:02:45 +0800

    Ui class: Enhance alignment with coding standards
    This commit improves the Ui class by addressing formatting
    inconsistencies and unclear documentation.
    
    Key updates include:
    - Refining Javadoc comments for the constructor and methods
      to make them more descriptive and informative.
    - Improve formatting, indentation, and layout to ensure consistency.
    - Introduce a static final constant, LINE_SEPARATOR, to eliminate
      repetitive string literals and enhance code maintainability.
    
    These changes make the Ui class easier to read, understand,
    and maintain while more closely adhering to the Java coding standards.

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,103 +1,106 @@
 public class Ui {
     private final String botName;
+    private static final String LINE_SEPARATOR =
+            "     ____________________________________________________________";
+
 
     /**
      * Constructor for Ui object for the given chatbot name.
      *
-     * @param botName the name of the chatbot.
+     * @param botName The name of the chatbot.
      */
     public Ui(String botName) {
         this.botName = botName;
     }
 
-    /*
+    /**
      * Returns the greeting.
      */
     public void printGreeting() {
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("     Hello! I'm " + botName);
         System.out.println("     What can I do for you?");
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
     }
 
-    /*
+    /**
      * Returns the exit message.
      */
     public void printExit() {
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("     Bye. Hope to see you again soon!");
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
     }
 
     /**
      * Echoes input by user back to the user.
      *
-     * @param input input by user to echo.
+     * @param input Input by user to echo.
      */
     public void echo(String input) {
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("     " + input);
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
     }
 
     /**
-     * Prints a message when task is added.
+     * Prints a message when the task is added.
      *
-     * @param task the task to add.
+     * @param task The task to add.
      */
-    public void printTaskAdded(String task, int taskcount) {
-        System.out.println("     ____________________________________________________________");
+    public void printTaskAdded(String task, int taskCount) {
+        System.out.println(LINE_SEPARATOR);
         System.out.println("     Got it. I've added this task:");
         System.out.println("       " + task);
-        System.out.println("     Now you have " + taskcount + " tasks in the list.");
-        System.out.println("     ____________________________________________________________");
+        System.out.println("     Now you have " + taskCount + " tasks in the list.");
+        System.out.println(LINE_SEPARATOR);
     }
 
 
     /**
      * Prints the list of tasks.
      *
-     * @param tasks an array of added tasks to display.
+     * @param tasks An array of added tasks to display.
      */
     public void printTaskList(Task[] tasks) {
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("     Here are the tasks in your list:");
         for (int i = 0; i < tasks.length; i++) {
             System.out.println("     " + (i + 1) + "." + tasks[i]);
         }
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
     }
 
 
     /**
      * Prints a message indicating that the task has been marked as done.
      *
-     * @param task the task that was marked as done.
+     * @param task The task that was marked as done.
      */
     public void printTaskMarked(Task task) {
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("     Nice! I've marked this task as done:");
         System.out.println("     " + task);
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
     }
 
     /**
      * Prints message indicating that the task has been marked as not done.
      *
-     * @param task the task that was marked as not done.
+     * @param task The task that was marked as not done.
      */
     public void printTaskUnmarked(Task task) {
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("     OK, I've marked this task as not done yet:");
         System.out.println("     " + task);
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
     }
 
 
     /**
-     * Prints messages when errors are encountered
+     * Prints messages when errors are encountered.
      *
-     * @param message the error message
+     * @param message The error message.
      */
     public void printErrorMessage(String message) {
         System.out.println("     WOOP WOOP!!! " + message);
@@ -106,14 +109,15 @@ public class Ui {
     /**
      * Prints message indicating a task is deleted.
      *
-     * @param task the task that was deleted.
-     * @param tasksSoFar the number of tasks that remains in the arraylist.
+     * @param task The task that was deleted.
+     * @param tasksSoFar The number of tasks that remains in the arraylist.
      */
     public void printTaskDeleted(Task task, int tasksSoFar) {
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("     Noted. I've removed this task:");
         System.out.println("       " + task);
         System.out.println("     Now you have " + tasksSoFar + " tasks in the list.");
-        System.out.println("     ____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
     }
+
 }
