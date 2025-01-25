ac251431443a61012c101488ceca001c98dd41b7
iamanoob44 <e1121904@u.nus.edu>
2025-01-25 14:14:41 +0800

    Shagbot: Enhance alignment with coding standards
    Improve the Shagbot class by addressing formatting inconsistencies
    and enhancing Javadoc comments for better adherence to coding
    standards.
    
    Changes include:
    - Refining Javadoc comments for the constructor and methods to ensure
      clarity and compliance with Java coding standards.
    - Standardise formatting, indentation, and layout for better
      readability.
    - Update method descriptions to better align with Java conventional
      documentation.
    
    These updates improve readability and compliance with coding standards.

--- src/main/java/Shagbot.java
+++ src/main/java/Shagbot.java
@@ -4,19 +4,19 @@ public class Shagbot {
 
     private final String botName;
     private final Ui ui;
-    private final ManageTasks manageTask;
+    private final ManageTasks manageTasks;
     private final Parser parser;
 
     /**
-     * Constructor for Shagbot
+     * Constructor for the Shagbot class.
      *
-     * @param name the name of the chatbot.
+     * @param name The name of the chatbot.
      */
     public Shagbot(String name) {
         botName = name;
         this.ui = new Ui(name);
-        this.manageTask = new ManageTasks();
-        this.parser = new Parser(manageTask, ui);
+        this.manageTasks = new ManageTasks();
+        this.parser = new Parser(manageTasks, ui);
     }
 
     /**
@@ -43,14 +43,14 @@ public class Shagbot {
     }
 
     /**
-     * Returns the greeting.
+     * Returns the greeting by the bot.
      */
     public static String printGreeting() {
         return "Hello! I'm Shagbot\n" + "What can I do for you?\n";
     }
 
-    /*
-     * Returns the exit message.
+    /**
+     * Returns the exit message by the bot.
      */
     public static String printExit() {
         return "Bye. Hope to see you again soon!\n";
