208cd131ef4f6eb543a9cb6c95cd405897708164
iamanoob44 <e1121904@u.nus.edu>
2025-01-25 14:44:58 +0800

    Todo class: Enhance alignment with coding standards
    Improve the Todo class by addressing formatting inconsistencies
    and enhancing Javadoc comments for better adherence to coding
    standards.
    
    Changes include:
    - Refining Javadoc comments for the constructor and the toString method
      to ensure clarity and compliance with Java coding standards.
    - Standardise formatting, indentation, and layout for better
      readability.
    - Update method descriptions to better align with Java conventional
      documentation.
    
    These updates improve readability and compliance with coding standards.

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,14 +1,21 @@
 public class Todo extends Task{
 
     /**
-     * Constructor for the Todo class
+     * Constructor for the Todo class.
      *
-     * @param desc the description of the task.
+     * @param desc The description of the task.
      */
     public Todo(String desc) {
         super(desc);
     }
 
+    /**
+     * Returns a string representation of the Todo Task.
+     * The format includes the task type "[T]" and the description from the
+     * parent Task class.
+     *
+     * @return A string representation of the Todo Task.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
