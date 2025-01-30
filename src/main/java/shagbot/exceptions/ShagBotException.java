2506b648669f4f2979d6e483cc88ffc58bde8921
iamanoob44 <e1121904@u.nus.edu>
2025-01-30 13:53:09 +0800

    Refine JavaDoc comments for this class

--- src/main/java/shagbot/exceptions/ShagBotException.java
+++ src/main/java/shagbot/exceptions/ShagBotException.java
@@ -3,8 +3,8 @@ package shagbot.exceptions;
 public class ShagBotException extends Exception{
 
     /**
-     * Constructor for the ShagBotException class for specified exceptions.
-     * This exception is thrown to indicate errors specific to ShagBot operations.
+     * Constructor for the {@code ShagBotException} class for specified exceptions.
+     * This exception is thrown to indicate errors specific to {@link shagbot.Shagbot} operations.
      *
      * @param message Error message displayed for exceptions.
      */
